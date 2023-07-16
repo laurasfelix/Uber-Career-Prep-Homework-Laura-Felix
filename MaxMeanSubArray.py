25ef1577d0a6d9550708e0c44e6229ab98e4ad5d
laurasfelix <laurasfelix@hotmail.com.br>
2023-07-15 20:19:31 -0500

    save

--- MaxMeanSubArray.py
+++
@@ -1,9 +0,0 @@
-def maxmean(nums, k):
-    sumy = -10000
-    for i in range(len(nums)-k+1):
-        if sum(nums[i:i+k]) > sumy:
-            sumy = sum(nums[i:i+k])
-    return sumy/k
-
-
-print(maxmean([1, 1, 1, 1, -1, -1, 2, -1, -1, 6], 5))
